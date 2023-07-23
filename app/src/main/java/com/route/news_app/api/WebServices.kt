96cbf8f5e7bdd35e485e91ba5f670bf31a414432
Abdelrahman Samy <abdelrahmansamy123>
2023-07-24 02:47:07 +0300

    modified apis to work with kotlin coroutines

--- app/src/main/java/com/route/news_app/api/WebServices.kt
+++ app/src/main/java/com/route/news_app/api/WebServices.kt
@@ -2,20 +2,19 @@ package com.route.news_app.api
 
 import com.route.news_app.newsResponse.NewsResponse
 import com.route.news_app.sourcesResponse.SourcesResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WebServices {
     @GET("v2/top-headlines/sources")
-    fun getSources(
+    suspend fun getSources(
         @Query("apiKey") apiKey: String,
         @Query("category") category: String
-    ): Call<SourcesResponse>
+    ): SourcesResponse
 
     @GET("v2/everything")
-    fun getNews(
+    suspend fun getNews(
         @Query("apiKey") apiKey: String, @Query("sources") sources: String,
         @Query("pageSize") pageSize: Int, @Query("page") page: Int,
-    ): Call<NewsResponse>
+    ): NewsResponse
 }
--- app/src/main/java/com/route/news_app/news/NewsViewModel.kt
+++ app/src/main/java/com/route/news_app/news/NewsViewModel.kt
@@ -2,14 +2,14 @@ package com.route.news_app.news
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.gson.Gson
 import com.route.news_app.api.ApiConstants
 import com.route.news_app.api.ApiManager
 import com.route.news_app.newsResponse.News
 import com.route.news_app.newsResponse.NewsResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class NewsViewModel : ViewModel() {
     val showLoading = MutableLiveData<Boolean>()
@@ -17,28 +17,48 @@ class NewsViewModel : ViewModel() {
     val newsList = MutableLiveData<List<News?>?>()
     fun getNews(sourceId: String, pageSize: Int, page: Int) {
         showLoading.value = true
-        ApiManager
-            .getApis()
-            .getNews(ApiConstants.apiKey, sources = sourceId, pageSize = pageSize, page = page)
-            .enqueue(object : Callback<NewsResponse> {
-                override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
-                    showError.value = t.localizedMessage
-                }
-
-                override fun onResponse(
-                    call: Call<NewsResponse>,
-                    response: Response<NewsResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        // we have news to show
-                        newsList.value = response.body()?.articles
-                        return
-                    }
-                    val errprResponse = Gson().fromJson(
-                        response.errorBody()?.string(), NewsResponse::class.java
+        viewModelScope.launch {
+            try {
+                val serverData = ApiManager
+                    .getApis()
+                    .getNews(
+                        ApiConstants.apiKey,
+                        sources = sourceId,
+                        pageSize = pageSize,
+                        page = page
                     )
-                    showError.value = errprResponse.message ?: ""
-                }
-            })
+                newsList.value = serverData.articles
+                showLoading.value = false
+            } catch (t: HttpException) {
+                val errorResponse = Gson().fromJson(
+                    t.response()?.errorBody()?.string(), NewsResponse::class.java
+                )
+                showError.value = errorResponse.message ?: ""
+            } catch (e: HttpException) {
+                showError.value = e.localizedMessage ?: ""
+            }
+
+        }
+
+//            .enqueue(object : Callback<NewsResponse> {
+//                override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+//                    showError.value = t.localizedMessage
+//                }
+//
+//                override fun onResponse(
+//                    call: Call<NewsResponse>,
+//                    response: Response<NewsResponse>
+//                ) {
+//                    if (response.isSuccessful) {
+//                        // we have news to show
+//                        newsList.value = response.body()?.articles
+//                        return
+//                    }
+//                    val errorResponse = Gson().fromJson(
+//                        response.errorBody()?.string(), NewsResponse::class.java
+//                    )
+//                    showError.value = errprResponse.message ?: ""
+//                }
+//            })
     }
 }
